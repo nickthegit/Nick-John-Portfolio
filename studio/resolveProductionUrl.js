812c46fbcd4c90006f1d716701a5b703d3721155
Nick <nick@rosielee.co.uk>
2022-08-04 22:05:50 +0100

    sort previews and split pane out

---
+++ studio/resolveProductionUrl.js
@@ -0,0 +1,28 @@
+const previewSecret = "7L2CnUSiomxLCeZWQw5fhw67y";
+
+// Replace `remoteUrl` with your deployed Next.js site
+const remoteUrl = `https://your-nextjs-site.com`;
+const localUrl = `http://localhost:3000`;
+
+export default function resolveProductionUrl(doc) {
+  const baseUrl =
+    window.location.hostname === "localhost" ? localUrl : remoteUrl;
+
+  const previewUrl = new URL(baseUrl);
+
+  let prepenedSlug = "";
+
+  if (doc._type === "post" && doc?.slug?.current) {
+    prepenedSlug = `work/${doc?.slug?.current}`;
+  } else if (doc?.slug?.current) {
+    prepenedSlug = `${doc?.slug?.current}`;
+  } else {
+    prepenedSlug = "/";
+  }
+
+  previewUrl.pathname = `/api/preview`;
+  previewUrl.searchParams.append(`secret`, previewSecret);
+  previewUrl.searchParams.append(`slug`, prepenedSlug ?? `/`);
+
+  return previewUrl.toString();
+}
