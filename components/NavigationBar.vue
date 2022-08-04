95c2fbd385afcb3dba61ecb760ff866000606f10
Nick <nick@rosielee.co.uk>
2022-08-04 09:58:38 +0100

    nuxt 3 start

---
+++ components/NavigationBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <div class="brand">
+      <NuxtLink to="/">Nick John</NuxtLink>
+    </div>
+    <ul>
+      <li>
+        <a href="#work">Work</a>
+      </li>
+      <li>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
