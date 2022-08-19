e9901f241baba5f73b7be91e63c90986b33b21d8
Nick <nick@rosielee.co.uk>
2022-08-19 22:02:01 +0100

    sorting homepage

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import "../styles/globals.css";
+import "../styles/all.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,9 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/pages/Home.module.scss";
 
 export default function Home() {
+  const clients = [
+    "StockX",
+    "Nike",
+    "Converse",
+    "Timberland",
+    "Vice",
+    "Itsu",
+    "Pepsi",
+    "Goal.Com",
+    "Havana Club",
+    "Marsheen",
+  ];
   return (
     <div>
       <Head>
@@ -15,28 +27,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="hero">
-          <h1>Creative Developer</h1>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <h1>
+            Front End <span>Developer</span>
+          </h1>
         </section>
-        <section className="about">
+        <article className={styles.about}>
           <p>
             I am a Front End Web Developer, based in the UK. Specialising in
             front-end technologies including CSS, HTML & Javascript. My stack
-            includes VueJS, React, Sass, Vuex, Wordpress, Jamstack, Next, Nuxt,
+            includes Vue.js, React, Sass, Vuex, Wordpress, Jamstack, Next, Nuxt,
             Shopify, WebGL and Git. I also have a lot of passion for great UI
             interactions, UX design and good semantics.
           </p>
+        </article>
+        <section className={styles.projects}>
+          <h2>Projects</h2>
+          <div className={styles.grid}>
+            <article className={styles.projectCard}>
+              <Link href="/projects/converse-renew-lab-store">
+                <a>
+                  <h3>Converse Renew Labs Store</h3>
+                  <img src="https://picsum.photos/1920/1080" alt="" />
+                </a>
+              </Link>
+            </article>
+            <article className={styles.projectCard}>
+              <Link href="/projects/converse-renew-lab-store">
+                <a>
+                  <h3>Converse Renew Labs Store</h3>
+                  <img src="https://picsum.photos/1920/1080" alt="" />
+                </a>
+              </Link>
+            </article>
+          </div>
         </section>
-        <h2>Projects</h2>
-        <Link href="/projects/converse-renew-lab-store">
-          <a>converse-renew-lab-store</a>
-        </Link>
+        <section className={styles.clients}>
+          <h2>Clients</h2>
+          <p>{clients.join(", ")}</p>
+        </section>
+        <footer>
+          <h3>Nick John</h3>
+          <a href="mailto:hello@nickjohn.co.uk">hello@nickjohn.co.uk</a>
+          <a href="https://github.com/nickthegit">Github</a>
+          <a href="https://www.linkedin.com/in/nick-john-a310a753/">Linkedin</a>
+        </footer>
       </main>
-
-      <footer>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </footer>
     </div>
   );
 }
