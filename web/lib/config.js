aa864a2f1d8b353beed590e66379bb9be39d6f74
Nick <nick@ournameismud.co.uk>
2023-03-20 09:44:40 +0000

    clear

--- web/lib/config.js
+++
@@ -1,8 +0,0 @@
-// lib/config.js
-export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: "2022-08-04", // Learn more: https://www.sanity.io/docs/api-versioning
-
-  useCdn: process.env.NODE_ENV === "production",
-};
